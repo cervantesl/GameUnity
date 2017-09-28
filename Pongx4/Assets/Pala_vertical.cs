904aedd9416ad6c383df36ce98d3ed703706d049
Lucas Cervantes <lucascervantesc@gmail.com>
2017-09-28 18:33:30 +0200

    c
    Comienzo

---
+++ Pongx4/Assets/Pala_vertical.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pala_vertical : MonoBehaviour {
+
+	public KeyCode arriba;
+	public KeyCode abajo;
+
+	public float speed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(arriba)){
+			transform.Translate (0, speed, 0);
+		}
+		if(Input.GetKey(abajo)){
+			transform.Translate (0, -speed, 0);
+		}
+	}
+}
